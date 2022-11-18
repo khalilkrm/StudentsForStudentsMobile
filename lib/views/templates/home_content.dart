0fe9224a2dfbf1625be86305079a40bd2445c971
Andrew Tistaert <a.tistaert@student.helmo.be>
2022-11-18 20:30:18 +0100

    MODIFY: modif home page

--- lib/views/templates/home_content.dart
+++ lib/views/templates/home_content.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:student_for_student_mobile/stores/home_store.dart';
+import 'package:student_for_student_mobile/views/molecules/request_accordion.dart';
 import 'package:student_for_student_mobile/views/organisms/screen_content.dart';
 
 class HomeContent extends StatefulWidget {
@@ -22,51 +23,27 @@ class _HomeContentState extends State<HomeContent> {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Container(
-                        width: width / 2,
-                        decoration: BoxDecoration(
-                          color: store.mode
-                              ? Colors.black
-                              : Colors.white,
-                          border: const Border(
-                            left:
-                                BorderSide(width: .5, color: Color(0xAA000000)),
-                            right:
-                                BorderSide(width: .5, color: Color(0xAA000000)),
-                            bottom:
-                                BorderSide(width: .5, color: Color(0xAA000000)),
-                          ),
-                        ),
+                      SizedBox(
+                        width: width / 3,
                         child: TextButton(
                             onPressed: () => store.requestsMode(),
                             child: Text('DEMANDES',
                                 style: TextStyle(
                                     color: store.mode
-                                        ? Colors.white
-                                        : Colors.black))),
+                                        ? Colors.black
+                                        : Colors.grey,
+                                    fontSize: 15))),
                       ),
-                      Container(
-                        width: width / 2,
-                        decoration: BoxDecoration(
-                          color: !store.mode
-                              ? Colors.black
-                              : Colors.white,
-                          border: const Border(
-                            left:
-                                BorderSide(width: .5, color: Color(0xAA000000)),
-                            right:
-                                BorderSide(width: .5, color: Color(0xAA000000)),
-                            bottom:
-                                BorderSide(width: .5, color: Color(0xAA000000)),
-                          ),
-                        ),
+                      SizedBox(
+                        width: width / 3,
                         child: TextButton(
                             onPressed: () => store.synthesesMode(),
                             child: Text('SYNTHESES',
                                 style: TextStyle(
                                     color: !store.mode
-                                        ? Colors.white
-                                        : Colors.black))),
+                                        ? Colors.black
+                                        : Colors.grey,
+                                    fontSize: 15))),
                       ),
                     ],
                   ),
