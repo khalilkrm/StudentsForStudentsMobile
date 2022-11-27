d553844236ee06dd74bb7facbebc359461bfd3ad
khalilkrm <52861234+khalilkrm@users.noreply.github.com>
2022-11-27 19:53:20 +0100

    ADD: chat feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
+import 'package:student_for_student_mobile/apis/firebase_api.dart';
 import 'package:student_for_student_mobile/apis/google_map_api.dart';
 import 'package:student_for_student_mobile/apis/home_api.dart';
 import 'package:student_for_student_mobile/apis/horairix_api.dart';
 import 'package:student_for_student_mobile/apis/request_api.dart';
 import 'package:student_for_student_mobile/apis/user_api.dart';
+import 'package:student_for_student_mobile/repositories/chat_repository.dart';
 import 'package:student_for_student_mobile/repositories/home_repository.dart';
 import 'package:student_for_student_mobile/repositories/horairix_repository.dart';
 import 'package:student_for_student_mobile/repositories/request_repository.dart';
 import 'package:student_for_student_mobile/repositories/user_repository.dart';
 import 'package:student_for_student_mobile/stores/calendar_store.dart';
+import 'package:student_for_student_mobile/stores/chat_store.dart';
 import 'package:student_for_student_mobile/stores/home_store.dart';
 import 'package:student_for_student_mobile/stores/map_store.dart';
 import 'package:student_for_student_mobile/stores/nav_store.dart';
@@ -23,12 +27,14 @@ import 'package:student_for_student_mobile/views/pages/authentication_page.dart'
 import 'package:student_for_student_mobile/views/pages/calendar_page.dart';
 import 'package:student_for_student_mobile/views/pages/chat_page.dart';
 import 'package:student_for_student_mobile/views/pages/home_page.dart';
-import 'package:student_for_student_mobile/views/pages/map_page.dart';
 import 'package:student_for_student_mobile/views/pages/profile_page.dart';
 import 'package:student_for_student_mobile/views/pages/requests_page.dart';
 import 'package:syncfusion_localizations/syncfusion_localizations.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   HttpOverrides.global = MyHttpOverrides();
 
   // google oauth openid scopes
@@ -40,6 +46,7 @@ Future<void> main() async {
   final RequestApi requestApi = RequestApi();
   final HomeApi homeApi = HomeApi();
   final GoogleMapApi googleMapApi = GoogleMapApi();
+  final FirebaseApi firebaseApi = FirebaseApi();
 
   // repositories
   final UserRepository userRepository = UserRepository(userApi: userApi);
@@ -48,6 +55,7 @@ Future<void> main() async {
   final RequestRepository requestRepository =
       RequestRepository(requestApi: requestApi);
   final HomeRepository homeRepository = HomeRepository(homeApi: homeApi);
+  final ChatRepository chatRepository = ChatRepository(api: firebaseApi);
 
   userApi.setUserRepository(userRepository);
   userApi.setHorairixRepository(horairixRepository);
@@ -60,6 +68,7 @@ Future<void> main() async {
     userRepository: userRepository,
     googleSignIn: GoogleSignIn(scopes: scopes),
   );
+
   final CalendarStore calendarStore =
       CalendarStore(horairixRepository: horairixRepository);
 
@@ -73,6 +82,8 @@ Future<void> main() async {
 
   final MapStore mapStore = MapStore(api: googleMapApi);
 
+  final ChatStore chatStore = ChatStore(repository: chatRepository);
+
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(create: (_) => userStore),
@@ -81,6 +92,7 @@ Future<void> main() async {
       ChangeNotifierProvider(create: (_) => requestStore),
       ChangeNotifierProvider(create: (_) => homeStore),
       ChangeNotifierProvider(create: (_) => mapStore),
+      ChangeNotifierProvider(create: (_) => chatStore),
     ],
     child: const MyApp(),
   ));
@@ -129,8 +141,6 @@ class MyApp extends StatelessWidget {
                     return const ProfilePage();
                   case 4:
                     return const CalendarPage();
-                  case 5:
-                    return const MapPage(destination: 'Barcelona');
                   default:
                     return const Text('Default');
                 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import cloud_firestore
+import firebase_core
 import location
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
